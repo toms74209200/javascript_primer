1761e1f4b807a0ed5c0f09af27142d4678855911
toms74209200 <motomotomato@gmail.com>
2021-01-18 21:56:24 +0900

    [add]add encoding

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ const fs = require("fs");
 program.parse(process.argv);
 
 const filePath = program.args[0];
-fs.readFile(filePath, (err, file) => {
+fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
     console.log(file);
 });
