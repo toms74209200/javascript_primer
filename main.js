d565ec4aec98fe4ee4f2d4e7c4089d84591f2f15
toms74209200 <motomotomato@gmail.com>
2021-01-18 22:01:36 +0900

    [add]add markdown library

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ console.log(myModule.foo);
 
 const program = require("commander");
 const fs = require("fs");
+const marked = require("marked");
 
 program.parse(process.argv);
 
@@ -13,5 +14,6 @@ fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
         process.exit(1);
         return;
     }
-    console.log(file);
+    const html = marked(file);
+    console.log(html);
 });
