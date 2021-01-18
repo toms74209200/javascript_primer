d5be2719766ebfebe703f688d0adfdbcb9f1162a
toms74209200 <motomotomato@gmail.com>
2021-01-18 22:20:42 +0900

    [add]add comandline option

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const program = require("commander");
 const fs = require("fs");
 const marked = require("marked");
 
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
+const options = program.opts();
+console.log(options.gfm);
 
 const filePath = program.args[0];
 fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
