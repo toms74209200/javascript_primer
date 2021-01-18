ccc8e313533b2436d21477f11a2d3fa9c48e56a4
toms74209200 <motomotomato@gmail.com>
2021-01-18 21:51:31 +0900

    [add]add parse of commandline args

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-console.log(process.argv);
-
 const myModule = require("./my-module");
 console.log(myModule.foo);
+
+const program = require("commander");
+program.parse(process.argv);
+
+const filePath = program.args[0];
+console.log(filePath);
